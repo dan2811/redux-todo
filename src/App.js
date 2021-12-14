ed1420cd8fedf8161e0474bb40cd11486e1a9a7d
dan2811 <djordandrums@gmail.com>
2021-12-14 14:38:18 +0000

    map todos and improve CSS

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import Input from './Components/Input';
 import './App.css';
+import TodoItem from './Components/TodoItem';
+
+const todoList = [{
+  name: 'todo',
+  done: false,
+  id: 123649873498734
+}, {
+  name: 'todo2',
+  done: true,
+  id: 987498733
+}];
+
 
 function App() {
   return (
     <div className="App">
       <div className="app__container">
         <div className="app__todoContainer">
-
+        {
+          todoList.map(item => (
+            <TodoItem 
+            name={item.name}
+            done={item.done}
+            id={item.id}
+            />
+          ))
+        }
         </div>
         <Input />
       </div>
